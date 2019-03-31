50912be276887182bfc4c502a7d5798ae441528a
Jordan Force <jordan.force@uconn.edu>
2019-03-31 12:38:26 -0400

    Almost done with script to create contact map. Just need to output the results

--- feature_engineering/contact_creator.py
+++ feature_engineering/contact_creator.py
@@ -1,11 +1,17 @@
 from Bio.PDB import *
 from Bio import pairwise2
 from Bio.SubsMat import MatrixInfo as matlist
+from Bio.Align.Applications import ClustalOmegaCommandline
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from Bio import AlignIO
 import os
 import os.path
 import urllib.request
 from collections import *
+import math
+
 
 def get_uniprot(uniprot_id):
     if ':' in uniprot_id:
@@ -30,50 +36,114 @@ def get_identity_score(seq_one, seq_two):
                 num_matches += 1
     return (num_overlap, num_matches*1.0/num_overlap)
 
-def get_sequence_
+class PositionMapper:
+    def __init__(self, reference_sequence):
+        self.mapper = None
+        self.reference_sequence = reference_sequence
+        #map PDB ID to sequence of the chain
+        self.chain_sequences = {}
 
-hla_sequence = 'GALALTQTWAGSHSMRYFFTSVSRPGRGEPRFIAVGYVDDTQFVRFDSDAASQKMEPRAPWIEQEGPEYWDQETRNMKAHSQTDRANLGTLRGYYNQSEDGSHTIQIMYGCDVGPDGRFLRGYRQDAYDGKDYIALNEDLRSWTAADMAAQITKRKWEAVHAAEQRRVYLEGRCVDGLRRYLENGKETLQRTDPPKTHMTHHPISDHEATLRCWALGFYPAEITLTWQRDGEDQTQDTELVETRPAGDGTFQKWAAVVVPSGEEQRYTCHVQHEGLPKPLTLRWELSSQPTIPIVGIIAGLVLLGAVITGAVVAAVMWRRKSSDRKGGSYTQAASSDSAQGSDVSLTACKV'
+    def add_chain(self, pdb_id, sequence):
+        self.chain_sequences[pdb_id] = sequence
+
+    def run_alignment(self, sequences_file, clustal_output_file):
+        """
+        The sequences_file is the name of the FASTA file to store the sequences to
+        The clustal_output_file is where Clustal Omega should write the output to
+        """
+        seq_records = [SeqRecord(Seq(sequence), id=key) for key, sequence in self.chain_sequences.items()] + [SeqRecord(Seq(self.reference_sequence), id='reference')]
+        with open(sequences_file, 'w') as output_handler:
+            SeqIO.write(seq_records, output_handler, 'fasta')
+            
+        clustalo = ClustalOmegaCommandline(infile=sequences_file, outfile=clustal_output_file, auto=True, verbose=True, force=True, infmt='fasta', outfmt='clustal')
+        print(clustalo)
+        clustalo()
+        alignment = AlignIO.read(clustal_output_file, 'clustal')
+        aligned_sequences = {record.id: str(record.seq) for record in alignment}
+        reference_seq = aligned_sequences['reference']
+        print('reference seq: %s' % reference_seq)
+        assert('-' not in reference_seq.strip('-'))
+        chain_counters = {id: 0 for id in aligned_sequences.keys()}
+        del aligned_sequences['reference']
+        self.mapper = {id: {} for id in aligned_sequences.keys()}
+        for i in range(0, len(reference_seq)):
+            for pdb_id, sequence in aligned_sequences.items():
+                assert(pdb_id is not 'reference')
+                if sequence[i] is not '-':
+                    self.mapper[pdb_id][i] = chain_counters['reference']
+                    chain_counters[pdb_id] += i
+            if reference_seq[i] is not '-':
+                chain_counters['reference'] += 0
+    def get_reference_position(self, i, pdb_id):
+        return self.mapper[pdb_id][i]
+                
+
+hla_sequence = 'GSHSMRYFFTSVSRPGRGEPRFIAVGYVDDTQFVRFDSDAASQKMEPRAPWIEQEGPEYWDQETRNMKAHSQTDRANLGTLRGYYNQSEDGSHTIQIMYGCDVGPDGRFLRGYRQDAYDGKDYIALNEDLRSWTAADMAAQITKRKWEAVHAAEQRRVYLEGRCVDGLRRYLENGKETLQRTDPPKTHMTHHPISDHEATLRCWALGFYPAEITLTWQRDGEDQTQDTELVETRPAGDGTFQKWAAVVVPSGEEQRYTCHVQHEGLPKPLTLRWELSSQPTIPIVGIIAGLVLLGAVITGAVVAAVMWRRKSSDRKGGSYTQAASSDSAQGSDVSLTACKV'
 
 matrix = matlist.blosum62
-
-position_dict = defaultdict(list)
-
-
+mapper = PositionMapper(hla_sequence)
+hla_chains = {} #map PDB id to the ID of the pocket chain
+peptide_chains = {}
+peptide_lengths = [8, 9, 10, 11, 12, 13]
+contact_map = {i: set() for i in peptide_lengths}
+"""
+First, get all of the chain sequences together, and do the multiple alignment
+"""
+z = 0
 with open('structures.txt', 'r') as f:
     for line in f:
         line = line.strip()
         if not line.startswith('#') and len(line) > 1:
+            """
+            if z >= 10:
+                break
+            z += 1
+            """
             p = PDBParser()
             structure = p.get_structure('X', os.path.join('structures', 'pdb' + line.lower() + '.ent'))
             chains = structure.get_chains()
 
-            best_chain = None
+            best_chain_sequence = None
             best_identity_score = 0
             best_alignment = None
             chain_list = list(chains)
             peptide_chain = None
+            peptide_chain_sequence = None
             peptide_length = 0
             chain_dict = {x.id: x for x in chain_list}
+            chain_sequences = {}
             manual_peptide_chain = False
             for chain in chain_list:
-                sequence = PPBuilder().build_peptides(chain)[0].get_sequence()
-                if len(sequence) in [8, 9, 10, 11, 12, 13] and manual_peptide_chain is False:
+                sequence = str(PPBuilder().build_peptides(chain)[0].get_sequence())
+                chain_sequences[chain.id] = sequence
+                if len(sequence) in peptide_lengths and manual_peptide_chain is False and sequence != peptide_chain_sequence:
                     peptide_length = len(sequence)
-                    if peptide_chain is not None:
+                    if peptide_chain is not None and sequence:
                         print('pdb id: ' + line.lower())
                         peptide_chain_id = input('What\'s the peptide chain? Here\'s a list: ' + ', '.join([x.id for x in chain_list]))
                         peptide_chain = chain_dict[peptide_chain_id]
+                        peptide_chain_sequence = sequence
                         manual_peptide_chain = True
                     else:
+                        peptide_chain_sequence = sequence
                         peptide_chain = chain
                 alignment = pairwise2.align.localds(hla_sequence, sequence, matrix, -10, -0.5, one_alignment_only = True)
                 num_overlap, identity_score = get_identity_score(alignment[0][0], alignment[0][1])
                 print('identity score: %f' % identity_score)
-                print('num overlap: %d', num_overlap)
+                print('num overlap: %d' % num_overlap)
+                print('Length: %d' % len(sequence))
                 if num_overlap >= 200 and identity_score > best_identity_score:                    
-                    best_chain = chain
+                    best_chain_sequence = sequence
+                    best_chain_id = chain.id
                     best_identity_score = identity_score
                     best_alignment = alignment
+            if peptide_chain is None:
+                print('pdb id: ' + line.lower())
+                peptide_chain_id = input('What\'s the peptide chain? Here\'s a list: ' + ', '.join([x.id for x in chain_list]))
+                peptide_chain = chain_dict[peptide_chain_id]
+                peptide_chain_sequence = sequence
+                manual_peptide_chain = True
+            peptide_chains[line.lower()] = peptide_chain.id
             print(best_alignment)
             if best_identity_score < 0.80:
                 print('PDB ID: ' + line.lower())
@@ -81,47 +151,42 @@ with open('structures.txt', 'r') as f:
                 while True:
                     chain_id = input('What\'s the best chain? Here is a list: ' + ', '.join([x.id for x in chain_list]) + ': ')                                    
                     if chain_id in chain_dict:
-                        best_chain = chain_dict[chain_id]
+                        best_chain_sequence = chain_sequences[chain_id]
+                        best_chain_id = chain_id
                         break
+            mapper.add_chain(line.lower(), best_chain_sequence)
+            hla_chains[line.lower()] = best_chain_id
+            
+mapper.run_alignment('sequences.fasta', 'clustal_output_file.clustal')
+z = 0            
+with open('structures.txt', 'r') as f:
+    for line in f:
+        line = line.strip()
+        if not line.startswith('#') and len(line) > 1:
+            """
+            if z >= 10:
+                break
+            z += 1
+            """
+            p = PDBParser()
+            pdb_id = line.lower()
+            structure = p.get_structure('X', os.path.join('structures', 'pdb' + pdb_id + '.ent'))
+            chains = {x.id: x for x in structure.get_chains()}
+            hla_chain = chains[hla_chains[pdb_id]]
+            peptide_chain = chains[peptide_chains[pdb_id]]
             """
             Now that we have both the peptide and HLA chains, and the alignment, iterate 
             """
-            peptide_residues = peptide_chain.get_residues()
-            hla_residues = best_chain.get_residues()
-            for peptide_residue in peptide_residues:
-                for hla_residue in hla_residues:
-                    if 'CA' in peptide_residue and 'CA' in hla_residue:
-                        print(peptide_residue['CA'] - hla_residue['CA'])
+            peptide_residues = str(peptide_chain.get_residues())
+            peptide_length = len(peptide_residues)
+            if peptide_length in peptide_lengths:
+                hla_residues = str(hla_chain.get_residues())
+                i = 0
+                for peptide_residue in peptide_residues:
+                    j = 0
+                    for hla_residue in hla_residues:
+                        if 'CA' in peptide_residue and 'CA' in hla_residue and abs(peptide_residue['CA'] - hla_residue['CA']) <= 4:
+                            reference_position = mapper.get_reference_position(j, pdb_id)
+                            contact_map[peptide_length].add((i, reference_position))
                 
-                
-                
-                
-            """
-            best_alignment_score = 0
-            best_alignment = None
-            for record in SeqIO.parse(os.path.join('structures', 'pdb' + line.lower() + '.ent'), 'pdb-seqres'):
-                if 'UNP' in record.dbxrefs[0]:
-                    sequence = get_uniprot(record.dbxrefs[0])
-                    
-                    
-                    
-                if record.annotations['chain'] == 'A':
-                    print(record.id)
-                    print('database refs: ')
-                    print(record.dbxrefs)
-                    has_a = True
-                    assert('UNP' in record.dbxrefs[0])
-            assert(has_a)
-            """
-            
-            #print('line: %s' % line)
-            #for chain in chains:
-            #    sequence = PPBuilder().build_peptides(chain)[0].get_sequence()
-            #    alignment = pairwise2.align.localxx(sequence, hla_sequence)[0]
-                
-                
-            #print('line: %s, chain lengths: %s' % (line, ', '.join([str(len(list(x.get_residues()))) for x in chains])))
-            
-            #ppb = PPBuilder()
-            #peptides = sorted([str(x.get_sequence()) for x in ppb.build_peptides(structure)], key=lambda x:abs(len(x) -9))
             
